103f3657063b086ade9068764e6462fdcd216af4
Enrique Jimenez <enrique.jimenez@bosonit.com>
2022-05-24 10:44:37 +0200

    Objetos Persona creados

---
+++ src/main/java/Objetos/Persona.java
@@ -0,0 +1,16 @@
+package Objetos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Persona {
+    private int id;
+    private String nombre;
+    private String edad;
+    private String ciudad;
+
+}
