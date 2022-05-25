6ada2131e39ba93bd7a271e034b48c4f19530e7c
Enrique Jimenez <enrique.jimenez@bosonit.com>
2022-05-25 10:30:38 +0200

    Modificado AppStartUp2--Imprimir Args

--- src/main/java/com/example/demo/AppStartUp2.java
+++ src/main/java/com/example/demo/AppStartUp2.java
@@ -5,9 +5,12 @@ import org.springframework.boot.CommandLineRunner;
 public class AppStartUp2 implements CommandLineRunner {
 
     @Override
-    public void run(String... args) throws Exception{
+    public void run(String...args) throws Exception{
 
+        // los parametros los pasaremos directamente desde el terminal cuando ejecutemos el .jar creo jeje
         System.out.println("Hello from the third class");
+        for (int i = 0; i < args.length;++i) {
+            System.out.println(args[i]);
+        }
     }
-
 }
